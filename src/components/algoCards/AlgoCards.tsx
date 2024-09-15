f3a20ae4ea8826949e34258d2533dc5f46b94b64
Himanshu Jain <jainh246@gmail.com>
2024-09-15 16:33:57 +0530

    fix styling and add selection sort metadata

--- src/components/algoCards/AlgoCards.tsx
+++ src/components/algoCards/AlgoCards.tsx
@@ -8,10 +8,16 @@ const ALGORITHMS = [
     url: "/bubble-sort",
     imageSrc: "/images/bubbleSort.jpg",
   },
+  {
+    name: "Selection Sort",
+    id: "selectionSort",
+    url: "/selection-sort",
+    imageSrc: "/images/selectionSort.jpg",
+  },
 ];
 
 export const AlgoCards = () => (
-  <div className="flex gap-4 p-10">
+  <div className="flex gap-12 p-10">
     {ALGORITHMS.map(({ name, id, url, imageSrc }) => (
       <AlgoCard key={id} name={name} url={url} imageSrc={imageSrc} />
     ))}
